0c3ebef8ddc58bf973b27f75029bb76a4310acdf
ChaewonKim25 <67187110+ChaewonKim25@users.noreply.github.com>
2020-06-28 13:41:46 +0900

    issue #2

--- template.py
+++ template.py
@@ -1,12 +1,12 @@
 html = b"""
 <html>
     <body>
-        <form oninput="sum.value=parseInt(a.value)+parseInt(b.value),product.value=parseInt(a.value)*parseInt(b.value)">
+        <form action="">
             a = <input type="number" name="a"> 
             b = <input type="number" name="b"><br><br>
             <input type="submit">
-            Sum: <output name="sum" for="a b"></output>
-            Product:<output name="product" value="parseInt(a.value)*parseInt(b.value)"></output>
         </form>
+            Sum(a+b): %(sum)d <br><br>
+            Product(a*b): %(product)d
     </body>
 """
