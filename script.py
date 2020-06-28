3e2351ae266daeef6cbcb3f0ae05c4fea7a2cc03
ChaewonKim25 <67187110+ChaewonKim25@users.noreply.github.com>
2020-06-28 13:42:33 +0900

    issue #2

--- script.py
+++ script.py
@@ -5,11 +5,12 @@ def application(environ,start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
+    x, y = 0, 0
     if '' not in [a,b]:
         a,b = int(a),int(b)
-        x = [a + b]
-        y = [a * b]
-    response_body = html
+        x = a + b
+        y = a * b
+    response_body = html % {'sum': x, 'product': y}
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
         ('Content-Length', str(len(response_body)))
